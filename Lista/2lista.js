43e6a2b18229607e54f74f821b7c0c0b031e6701
ErickSdeP <ericksantosdep@gmail.com>
2025-10-02 21:38:39 -0300

    Corrigindo 2

--- Lista/2lista.js
+++ Lista/2lista.js
@@ -3,13 +3,13 @@ const peso = 120;
 
 const imc = peso / (altura * altura);
 
-if (imc <= 18.5) {
+if (imc < 18.5) {
     console.log('Abaixo do peso');
-} else if (imc > 18.5 && imc <= 25) {
+} else if (imc >= 18.5 && imc < 25) {
     console.log('Peso normal');
-} else if (imc > 25 && imc <= 30) {
+} else if (imc >= 25 && imc < 30) {
     console.log('Acima do peso');
-} else if (imc > 30 && imc <= 40) {
+} else if (imc >= 30 && imc < 40) {
     console.log('Obeso');
 } else {
     console.log('Obesidade grave');
