62f857c2a933a078288d7294e19bef96e1818302
ErickSdeP <ericksantosdep@gmail.com>
2025-10-02 22:56:26 -0300

    Reorganizando minha pasta

---
+++ Segundo módulo/Aulas/3aula.js
@@ -0,0 +1,12 @@
+function teste() {
+    console.log('teste')
+}
+teste()
+
+function yourName(nome) {
+    console.log('Seu nome é: ' + nome)
+}
+
+yourName('Erick');
+yourName('Amanda');
+yourName('Zaza');
