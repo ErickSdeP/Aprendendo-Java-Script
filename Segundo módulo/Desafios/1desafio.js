e8ce5644e52113164ce217a1ef0e4f20c402bbb3
ErickSdeP <ericksantosdep@gmail.com>
2025-10-31 11:22:11 -0300

    N sei

--- Segundo módulo/Desafios/1desafio.js
+++ Segundo módulo/Desafios/1desafio.js
@@ -3,17 +3,17 @@ function imcCalc(peso, altura) {
 }
 
 function classe(imc) {
-    if (imc < 18.5) {
-    return 'Abaixo do peso';
-} else if (imc >= 18.5 && imc < 25) {
-    return 'Peso normal';
-} else if (imc >= 25 && imc < 30) {
-    return 'Acima do peso';
-} else if (imc >= 30 && imc < 40) {
-    return 'Obeso';
-} else {
-    return 'Obesidade grave';
-}
+        if (imc < 18.5) {
+        return 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc < 25) {
+        return 'Peso normal';
+    } else if (imc >= 25 && imc < 30) {
+        return 'Acima do peso';
+    } else if (imc >= 30 && imc < 40) {
+        return 'Obeso';
+    } else {
+        return 'Obesidade grave';
+    }
 }
 
 (function /*main*/() {
