2e1dbd13e27679899b54944b5194750bc60beabc
ErickSdeP <ericksantosdep@gmail.com>
2025-12-02 18:10:10 -0300

    entendendo const

---
+++ Terceiro módulo/Aulas/1aula.js
@@ -0,0 +1,22 @@
+const Erick = {
+    nome: 'Erick Santos',
+    idade: 17,
+
+    descrever: function() {
+        console.log(`Meu nome é ${this.nome} e eu tenho ${this.idade} anos de idade`)
+    }
+}
+
+Erick.descrever();
+
+
+
+
+/*
+Erick.altura = 1.85;
+console.log(Erick.nome)
+console.log(Erick.idade)
+delete Erick.idade //Caso n queira mais
+console.log(Erick) //Mesmo com a autura fora dos parênteses o program entende q faz parte
+console.log(Erick.altura)
+*/
