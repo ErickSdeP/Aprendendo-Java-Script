ff74ee4b3e8e14b74f3bae8cb440614413a8881d
ErickSdeP <ericksantosdep@gmail.com>
2025-12-17 16:23:10 -0300

    teste

---
+++ Terceiro módulo/Aulas/3aula.js
@@ -0,0 +1,22 @@
+class Pessoa {
+    nome;
+    idade;
+    anoNascimento;
+    anoAtual = 2025;
+
+    constructor(nome, idade) { 
+        this.nome = nome;
+        this.idade = idade;
+        this.anoNascimento = this.anoAtual - idade;
+    }
+
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e eu tenho ${this.idade} anos de idade`)
+    }
+}
+
+const Erick = new Pessoa('Erick Santos', 17);
+delete Erick.anoAtual
+
+Erick.descrever();
+console.log(Erick);
