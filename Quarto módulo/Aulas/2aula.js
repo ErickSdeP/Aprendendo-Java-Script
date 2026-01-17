f034a07fd05ee9e8c550b625dae5a1501c199f04
ErickSdeP <ericksantosdep@gmail.com>
2026-01-17 18:18:08 -0300

    explicação passo a passo do "for"

--- Quarto módulo/Aulas/2aula.js
+++ Quarto módulo/Aulas/2aula.js
@@ -1,9 +1,19 @@
 
+const nome = 'Erick Santos de Paula'
 
 
 
-
-
-for (let i = 0; i <= 10; i++) {
-    console.log(i)
+for (let i = 3; i < nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra);
 }
+
+
+/*
+for (let index = 0;                (Começando pelo 0 diz onde começa, sendo 0 o "E" do meu nome e o 2 o "i")
+    index < array.length;          (array.length = ao numero maximo (mostrar na próxima aula))
+    index++) {                     (está falando q vai somar...)
+    
+    const element = array[index];  (oq vai somar)
+}
+*/
