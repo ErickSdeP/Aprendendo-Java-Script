177b033f316420a54b698b2873a3665c03e8f5cc
ErickSdeP <ericksantosdep@gmail.com>
2026-01-22 20:02:52 -0300

    tabuada feita :)

--- Quarto módulo/Exercícios/2exercício.js
+++ Quarto módulo/Exercícios/2exercício.js
@@ -1,1 +1,9 @@
 //Crie um programa que dado um numero imprima a sua tabuada.
+
+for (let i = 1; i <= 10; i++) {
+    for (let n = 0; n <= 10; n++) {
+        let tabuada = i * n;
+        
+        console.log(tabuada);
+    }
+}
