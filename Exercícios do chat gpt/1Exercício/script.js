9c5778bf91a3035d9268d1b59aa426cc6bcdab4f
ErickSdeP <ericksantosdep@gmail.com>
2026-01-30 00:50:39 -0300

    corrigindo

--- Exercícios do chat gpt/1Exercício/script.js
+++ Exercícios do chat gpt/1Exercício/script.js
@@ -1,4 +1,6 @@
 const nome = 'Erick';
 const idade = 17;
 
-console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos')
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
+//console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos')
